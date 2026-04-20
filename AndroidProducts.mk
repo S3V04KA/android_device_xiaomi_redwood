feda92c3867a5cbaec3403a75427497356e55941
Sevka <sevstar2206@gmail.com>
2026-04-21 03:28:20 +0500

    fix

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_redwood.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_redwood-user \
-    lineage_redwood-userdebug \
-    lineage_redwood-eng
+    lineage_redwood-ap2a-user \
+    lineage_redwood-ap2a-userdebug \
+    lineage_redwood-ap2a-eng
